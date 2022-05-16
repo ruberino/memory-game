378aecf74d6b7b4bcf0e44da15ee30cc5bff1996
Terje Laugaland <terjel@aboveit.no>
2022-05-16 13:18:47 +0200

    Change game array to be generated programatically

--- src/utils.ts
+++ src/utils.ts
@@ -1,53 +1,21 @@
-import Img1 from './static/bilde2.png';
-import Img2 from './static/bilde3.png';
-import Img3 from './static/bilde15.png';
-import Img4 from './static/bilde13.png';
-import Img5 from './static/bilde14.png';
-import Img6 from './static/bilde4.png';
-import Backside from './static/backside.png';
+import { GameElement } from "./interface";
 
-export default [
-    Img1,
-    Img2,
-    Img3,
-    Img4,
-    Img5,
-    Img6,
-    Backside
-];
+import Img1 from "./static/bilde2.png";
+import Img2 from "./static/bilde3.png";
+import Img3 from "./static/bilde15.png";
+import Img4 from "./static/bilde13.png";
+import Img5 from "./static/bilde14.png";
+import Img6 from "./static/bilde4.png";
 
+const images = [Img1, Img2, Img3, Img4, Img5, Img6];
 
-export const gameArray = [{
+export const gameArray: Omit<GameElement, "id">[] = images.map(
+  (img, index) => ({
     locked: false,
-    imgId: 1,
-    img: Img1,
+    imgId: index + 1,
+    img,
     shown: false,
-    
-},{
-    locked: false,
-    imgId: 2,
-    img: Img2,
-    shown: false
-},{
-    locked: false,
-    imgId: 3,
-    img: Img3,
-    shown: false
-},{
-    img: Img4,
-    imgId: 4,
-    locked: false,
-    shown: false
-},{
-    img: Img5,
-    imgId: 5,
-    locked: false,
-    shown: false
-},{
-    img: Img6,
-    imgId: 6,
-    locked: false,
-    shown: false
-}]
+  })
+);
 
-export const getRandomNumber = () => (Math.random() > .5) ? 1 : -1
+export const getRandomNumber = () => (Math.random() > 0.5 ? 1 : -1);
