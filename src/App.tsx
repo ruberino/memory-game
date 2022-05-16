0ff0da7178d17884912fafc37f49997d99a66a00
Ruben <ruben.ringdal@kreftregisteret.no>
2022-05-16 11:49:19 +0200

    rest

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import { Children, useState } from 'react';
 import './App.css';
+import { GameElement } from './interface';
+import { gameArray, getRandomNumber } from './utils'
+import Backside from './static/backside.png';
+
+
 
 function App() {
+
+  const [chosenElement, setChosenElement ] = useState<GameElement>()
+  const initArray = [...gameArray, ...gameArray].map((element, i) => ({...element, id: i + 1}) ).slice().sort(getRandomNumber)
+  const [gamelist, setGamelist] = useState<GameElement[]>(initArray)
+  let timer: ReturnType<typeof setTimeout>;
+
+  const wrongElementsChosen = (el: GameElement) => {
+    setGamelist(prestate => prestate.map(x => {
+      return {
+        ...x,
+        shown: x.id === el.id || x.id === chosenElement?.id  ? true : false,
+        disabled: true
+      }
+    }))
+    timer = setTimeout(() => {
+      setChosenElement(undefined)
+      setGamelist(prestate => prestate.map(x => {
+        return {
+          ...x,
+          shown: false,
+          disabled: false
+        }
+      }))
+    }, 1000)
+  }
+
+  const handleElementClick = (el: GameElement) => {
+    if(!chosenElement){
+      setChosenElement(el);
+      setGamelist(prestate => prestate.map(x => {
+        return {
+          ...x,
+          shown: el.id === x.id ? true : false,
+        }
+      }))
+    }else if(chosenElement && el.imgId === chosenElement.imgId && el.id !== chosenElement.id) {
+      setGamelist(prestate => prestate.map(x => {
+        return {
+          ...x,
+          shown: false,
+          locked: el.id === x.id || chosenElement.id === x.id || x.locked ? true : false 
+        }
+      }))
+      setChosenElement(undefined)
+    }else {
+      wrongElementsChosen(el);
+    }
+  }
+  const restart = () => {
+    clearTimeout(timer);
+    setChosenElement(undefined);
+    setGamelist(initArray);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+        <h1>
+          MEMORY GAME
+        </h1>
       </header>
+      <main>
+        <button onClick={restart}>RESTART</button>
+        <div className="game-board" >
+          {
+            gamelist.map(x => (
+              <button disabled={x.disabled} onClick={() => handleElementClick(x)} key={x.id} className="game-element"> 
+                <img className="game-element-img" src={ x.shown || x.locked ?  x.img : Backside } />
+              </button>
+            )
+            )
+          }
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/interface.ts
@@ -0,0 +1,8 @@
+export type GameElement = {
+    id: number;
+    imgId: number;
+    img: string;
+    shown: boolean;
+    locked: boolean;
+    disabled?: boolean;
+  }
