1ef842922f97d7b5a29f43be089e178211dff913
Ruben <ruben.ringdal@kreftregisteret.no>
2022-05-31 15:56:03 +0200

    add css module for game board

--- src/components/gameboard/GameBoard.tsx
+++ src/components/gameboard/GameBoard.tsx
@@ -1,17 +1,17 @@
 import { GameElement } from "../../interface";
 import Backside from '../../static/backside.png';
-
+import style from './gameboard.module.css';
 type Props = {
     gamelist: GameElement[]
     handleElementClick: (el: GameElement) => void;
 }
 
 const GameBoard = ({gamelist, handleElementClick}: Props) => {
-    return <div className="game-board" >
+    return <div className={style.gameBoard} >
     {
       gamelist.map(x => (
-        <button disabled={x.disabled || x.locked || x.shown} onClick={() => handleElementClick(x)} key={x.id} className="game-element"> 
-          <img className="game-element-img" src={ x.shown || x.locked ?  x.img : Backside } alt={`item nr ${x.id}`} />
+        <button disabled={x.disabled || x.locked || x.shown} onClick={() => handleElementClick(x)} key={x.id} className={style.gameElement}> 
+          <img className={style.gameElementImg} src={ x.shown || x.locked ?  x.img : Backside } alt={`item nr ${x.id}`} />
         </button>
       )
       )  
